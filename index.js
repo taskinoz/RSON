eb9da31ba3cf243e7fff4d8f5bb62718a4a18a9b
taskinoz <tristan@taskinoz.com>
2023-09-07 00:48:08 +1000

    Added script to encode and decode rson

---
+++ index.js
@@ -0,0 +1,74 @@
+const fs = require('fs')
+const playlist = fs.readFileSync('./playlist.txt', 'utf-8')
+const testRSON = fs.readFileSync('./testRson.txt', 'utf-8')
+const testRSONComment = fs.readFileSync('./testRsonComment.txt', 'utf-8')
+
+function encodeRSON(obj, indent = '') {
+    let rson = '';
+
+    for (const [key, value] of Object.entries(obj)) {
+        if (typeof value === 'object') {
+            rson += `${indent}${key}\n${indent}{\n`;
+            rson += encodeRSON(value, indent + '\t');
+            rson += `${indent}}\n`;
+        } else {
+            rson += `${indent}${key} ${value}\n`;
+        }
+    }
+
+    return rson;
+}
+
+function parseRSON(text) {
+    const lines = text.split('\n');
+    const stack = [];
+    let currentObject = {};
+    const root = currentObject;
+
+    lines.forEach(line => {
+        const trimmedLine = line.trim();
+
+        if (trimmedLine.endsWith('{')) {
+            const key = trimmedLine.slice(0, -1).trim();
+            const newObject = {};
+            if (key) {
+                currentObject[key] = newObject;
+            } else {
+                const lastKey = Object.keys(currentObject).pop();
+                currentObject[lastKey] = newObject;
+            }
+            stack.push(currentObject);
+            currentObject = newObject;
+        } else if (trimmedLine === '}') {
+            currentObject = stack.pop();
+        } else if (trimmedLine) {
+            const [key, value = ''] = trimmedLine.split(/\s+(?=(?:[^"]*"[^"]*")*[^"]*$)/).map(str => str.trim());
+            currentObject[key] = isNaN(value) ? value.replace(/['"]+/g, '') : parseInt(value);
+        }
+    });
+
+    return root;
+}
+
+const data = {
+    playlists: {
+        version: 'stable',
+        versionNum: 3284,
+        Gamemodes: {
+            defaults: {
+                vars: {
+                    pve_menu: 0,
+                    enable_emotes: 0,
+                    boost_store_mode: 'off',
+                }
+            }
+        }
+    }
+};
+
+// const rsonText = encodeRSON(data);
+// console.log(rsonText);
+
+const rsonObj = parseRSON(testRSON);
+console.log(rsonObj);
+
