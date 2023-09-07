449698c48bd4f747cbb2fec600de87cbe1aeb560
taskinoz <tristan@taskinoz.com>
2023-09-07 21:24:22 +1000

    Added extract and strip comments

--- index.js
+++ index.js
@@ -33,23 +33,47 @@ function parseRSON(text) {
         if (trimmedLine.endsWith('{')) {
             const key = trimmedLine.slice(0, -1).trim();
             const newObject = {};
+
             if (key) {
                 currentObject[key] = newObject;
             } else {
+                // If there's no key before an opening brace, 
+                // we should merge the new object into the last key-value pair in the current object.
                 const lastKey = Object.keys(currentObject).pop();
                 currentObject[lastKey] = newObject;
             }
+
             stack.push(currentObject);
             currentObject = newObject;
         } else if (trimmedLine === '}') {
             currentObject = stack.pop();
-        } else if (trimmedLine) {
-            const [key, value = ''] = trimmedLine.split(/\s+(?=(?:[^"]*"[^"]*")*[^"]*$)/).map(str => str.trim());
-            currentObject[key] = isNaN(value) ? value.replace(/['"]+/g, '') : parseInt(value);
+        } else if (trimmedLine && !trimmedLine.endsWith('}')) {
+            const [key, value = '{}'] = trimmedLine.split(/\s+(?=(?:[^"]*"[^"]*")*[^"]*$)/).map(str => str.trim());
+            currentObject[key] = isNaN(value) ? (value === '{}' ? {} : value.replace(/['"]+/g, '')) : parseInt(value);
         }
     });
 
     return root;
+}
+
+function stripComments(text) {
+    return text.split('\n').map(line => {
+        const commentIndex = line.indexOf('//');
+        if (commentIndex !== -1) {
+            return line.substring(0, commentIndex).trim();
+        }
+        return line;
+    }).filter(line => line.trim() !== '').join('\n');
+}
+
+function extractComments(text) {
+    return text.split('\n').reduce((acc, line) => {
+        const commentIndex = line.indexOf('//');
+        if (commentIndex !== -1) {
+            acc.push(line.substring(commentIndex + 2).trim());
+        }
+        return acc;
+    }, []);
 }
 
 const data = {
@@ -71,6 +95,13 @@ const data = {
 // const rsonText = encodeRSON(data);
 // console.log(rsonText);
 
-const rsonObj = parseRSON(testRSON);
-console.log(JSON.stringify(rsonObj));
+// const rsonObj = parseRSON(playlist);
+// console.log(JSON.stringify(rsonObj));
 
+// fs.writeFileSync('./testfiles/testRsonExport.txt', JSON.stringify(rsonObj, true, 2));
+
+// const rsonObj = parseRSON(testRSON);
+// console.log(JSON.stringify(rsonObj, true, 2));
+
+const comments = extractComments(playlist);
+console.log(comments);
