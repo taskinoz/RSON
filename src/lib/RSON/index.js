e1b8843e24c6581bfd0e8bf38b39327e0f4e3980
taskinoz <tristan@taskinoz.com>
2023-09-25 20:51:52 +1000

    Updated RSON parser

--- src/lib/RSON/index.js
+++ src/lib/RSON/index.js
@@ -1,45 +1,75 @@
-function parseFile(fileContent) {
-    const sections = fileContent.split(/\n\n+/);
-    const parsedData = [];
+function parse(text) {
+    // Remove JavaScript-style comments
+    const cleanText = text.replace(/\/\/.*$/gm, '').trim();
 
-    sections.forEach(section => {
-        const conditionMatch = section.match(/When: "(.*?)"/);
-        if (conditionMatch) {
-            const condition = conditionMatch[1];
-            const scripts = Array.from(section.matchAll(/\t(.*\.gnut|.*\.nut)/g)).map(match => match[1]);
-            parsedData.push({
-                condition: condition,
-                scripts: scripts
-            });
+    const objectStrings = cleanText.split(/\}\s*\{/);
+    const parsedObjects = objectStrings.map((objectStr, index) => {
+        // Add double quotes around keys
+        let jsonObject = objectStr.replace(/(\w+)\s*:/g, '"$1":');
+
+        // Wrap string literals in double quotes, accounting for multiple dots
+        jsonObject = jsonObject.replace(/:(\s*)(\w+(\.\w+)+)/g, ': "$2"');
+
+        // Wrap any other non-quoted value in double quotes
+        jsonObject = jsonObject.replace(/:(\s*)([^"\{\}\[\],\s]+)/g, (match, p1, p2) => {
+            // Check if p2 is a number
+            if (/^\d+$/.test(p2)) {
+                return `:${p2}`;  // return as is for numbers
+            } else {
+                return `: "${p2}"`;  // wrap in quotes for other literals
+            }
+        });
+
+        // Insert commas between properties within each object
+        jsonObject = jsonObject.replace(/\"\s+\"/g, '", "');
+
+        // Ensure each object is wrapped in curly braces
+        if (!jsonObject.startsWith('{')) {
+            jsonObject = '{' + jsonObject;
         }
+        if (!jsonObject.endsWith('}')) {
+            jsonObject += '}';
+        }
+
+        console.log(jsonObject);
+
+        // Parse the processed JSON string
+        return JSON.parse(jsonObject);
     });
 
-    return parsedData;
+    return parsedObjects;
 }
 
-function encodeToFile(parsedData) {
-    return parsedData.map(section => {
-        const condition = section.condition;
-        const scripts = section.scripts.map(script => `\t${script}`).join('\n');
-        return `When: "${condition}"\nScripts:\n${scripts}`;
-    }).join('\n\n');
+
+
+function encode(obj) {
+    // Pull the opbjects from the array and convert them to strings
+    const strings = obj.map(o => JSON.stringify(o, null, '\t'));
+    // Join the strings together with newlines between them
+    const text = strings.join('\n');
+    // Remove the quotes around the keys but not the values
+    return text.replace(/"(\w+)":/g, '$1:') + '\n';
 }
 
-// Example usage:
-const fileContent = `
-... [your file content here] ...
-`;
+function strip(text) {
+    return text.replace(/\/\/.*$/gm, '').trim();
+}
 
-const parsedData = parseFile(fileContent);
-console.log(parsedData);
+function comments(text) {
+    const comments = [];
+    const regex = /\/\/(.*$)/gm;
+    let match;
 
-const encodedContent = encodeToFile(parsedData);
-console.log(encodedContent);
+    while ((match = regex.exec(text)) !== null) {
+        comments.push(match[1].trim());
+    }
 
+    return comments;
+}
 
-// module.exports = {
-//     encode,
-//     parse,
-//     strip,
-//     comments,
-// };
+module.exports = {
+    encode,
+    parse,
+    strip,
+    comments,
+};
